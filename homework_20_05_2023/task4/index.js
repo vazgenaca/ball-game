0211f10f74ba5f208c4a6551d2c9d3bfa6e768a4
vazgen <vazgentigranich@gmail.com>
2023-05-20 17:56:54 +0400

    keep changes

--- homework_20_05_2023/task4/index.js
+++ homework_20_05_2023/task4/index.js
@@ -1,27 +1,25 @@
 let button = document.querySelector("#changeRed");
 let squares = document.querySelectorAll(".square");
 
-
 let clc = true;
 button.addEventListener("click", (event) => {
   for (let a = 0; a < squares.length; a++) {
     if (a % 2 === 1) {
       squares[a].style.backgroundColor = "blue";
     }
-
-        if (clc) {
-            if (a % 2 === 0) {
-                squares[a].style.left = `${1400}px`;
-            } else {
-                squares[a].style.left = 0;
-            }
-        }  else {
-            if (a % 2 === 0) {
-                squares[a].style.left = 0;
-            } else {
-                squares[a].style.left = `${1400}px`;
-            }
-        }
+    if (clc) {
+      if (a % 2 === 0) {
+        squares[a].style.left = `${1400}px`;
+      } else {
+        squares[a].style.left = 0;
+      }
+    } else {
+      if (a % 2 === 0) {
+        squares[a].style.left = 0;
+      } else {
+        squares[a].style.left = `${1400}px`;
+      }
+    }
   }
   clc = !clc;
 });
