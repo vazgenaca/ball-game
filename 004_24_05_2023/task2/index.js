fcf8cc8f14719da0ff40239d67ec125942aa98c3
vazgen <vazgentigranich@gmail.com>
2023-05-24 21:00:03 +0400

    keep changes

---
+++ 004_24_05_2023/task2/index.js
@@ -0,0 +1,13 @@
+(function() {
+
+    // getUsers();
+    
+    // Get users returns an array of users, note that it is async function
+    // print users list in the html
+    // if user is admin the text color should be red
+    // if user is not admin text color should be green
+    // click button show only admins
+    // click button show only not admins
+    // click button show all
+    // click button add new user which is not admin
+})();
