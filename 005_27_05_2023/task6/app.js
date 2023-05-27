fe32c4bb396c8619a858043655e6d3ef7770bf3d
vazgen <vazgentigranich@gmail.com>
2023-05-27 17:49:35 +0400

    keep changes

--- 005_27_05_2023/task6/app.js
+++ 005_27_05_2023/task6/app.js
@@ -1,7 +1,24 @@
 // numbers should grow starts from 0 up to dataset value
 
 // HINT: example of getting dataset value
-const el = document.querySelector('.number');
-console.log(el.dataset.value)
+const elements = document.querySelectorAll('.number');
+
+
+Array.from(elements).forEach((element) => {
+    let count = 0;
+    let speed = 40;
+    let lastValue = +element.dataset.value;
+    const incTime = lastValue / speed;
+
+    let intervalId = setInterval(() => {
+        count += incTime;
+        element.innerHTML = Math.floor(count);
+        if (count >= +element.dataset.value) {
+            element.innerHTML = +element.dataset.value;
+            clearInterval(intervalId);
+        }
+    }, speed);
+})
+
 
 // use setInterval function
