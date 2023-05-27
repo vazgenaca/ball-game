3223c9d2092b17ff86640a300b228b2c13e3a120
vazgen <vazgentigranich@gmail.com>
2023-05-27 10:59:00 +0400

    keep changes

---
+++ 005_27_05_2023/task1/index.js
@@ -0,0 +1,27 @@
+// change div background colors with help of these classes
+
+const bgColors = [
+    "bg-primary",
+    "bg-primary-subtle",
+    "bg-secondary",
+    "bg-secondary-subtle",
+    "bg-success",
+    "bg-success-subtle",
+    "bg-danger",
+    "bg-danger-subtle",
+    "bg-warning",
+    "bg-warning-subtle",
+    "bg-info",
+    "bg-info-subtle",
+    "bg-light",
+    "bg-light-subtle",
+    "bg-dark",
+    "bg-dark-subtle",
+    "bg-body-secondary",
+    "bg-body-tertiary",
+    "bg-body",
+    "bg-black",
+    "bg-white",
+    "bg-transparent",
+  ];
+  
---
+++ 005_27_05_2023/task4/index.js
@@ -0,0 +1,3 @@
+// when number up to 0 set color green
+// when number less than 0 set color red
+// when number 0 no color
