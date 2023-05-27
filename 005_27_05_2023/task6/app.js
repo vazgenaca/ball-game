aec9fa71e4de2e420dcc58838f6dc2481da226cc
vazgen <vazgentigranich@gmail.com>
2023-05-27 10:54:57 +0400

    keep changes

---
+++ 005_27_05_2023/task6/app.js
@@ -0,0 +1,7 @@
+// numbers should grow starts from 0 up to dataset value
+
+// HINT: example of getting dataset value
+const el = document.querySelector('.number');
+console.log(el.dataset.value)
+
+// use setInterval function
