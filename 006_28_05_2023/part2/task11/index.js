0afffdde513c7e09b77b00822fdd2e21eca8c5ca
vazgen <vazgentigranich@gmail.com>
2023-05-28 15:58:51 +0400

    keep changes

---
+++ 006_28_05_2023/part2/task11/index.js
@@ -0,0 +1,9 @@
+
+/**
+ * iterate and print elements lower than 40 using "for (let x of arr)"
+ */
+
+const arr = [30, 42, 10, 21, 6, 0, 59, 81, 25, 30, 83, 79, 74, 14, 72, 19, 85, 40, 53, 8, 16, 60, 50];
+
+
+
