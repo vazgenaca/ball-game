d4596e908ce3b6a04e244ce38f77bc1accf93504
vazgen <vazgentigranich@gmail.com>
2023-05-28 16:08:12 +0400

    keep changes

---
+++ 006_28_05_2023/part3/task1/index.js
@@ -0,0 +1,23 @@
+
+/**
+ * fill function body
+ * return how many keys are in object. Use Object.keys.
+ */
+const obj = {
+    one: '1',
+    two: 2,
+    three: ['hi'],
+    four: {
+        five: null,
+    },
+    six: NaN,
+    seven: undefined,
+};
+
+function howManyKeysinObject(obj) {
+
+}
+
+console.log(howManyKeysinObject(obj));
+
+
---
+++ 006_28_05_2023/part3/task2/index.js
@@ -0,0 +1,23 @@
+
+/**
+ * function should return new array which should contain falsy values form the object.
+ */
+const obj = {
+    one: '1',
+    two: 2,
+    three: ['hi'],
+    four: {
+        five: null,
+    },
+    six: NaN,
+    seven: undefined,
+    eight: 0,
+};
+
+function getFalsyValues(obj) {
+
+}
+
+console.log(getFalsyValues(obj));
+
+
